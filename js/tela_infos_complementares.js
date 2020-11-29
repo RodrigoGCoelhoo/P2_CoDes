14a72b12c54ffea2d34ba017694088b2dd17f5c1
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2020-11-29 19:15:02 -0300

    Mudancas

--- js/tela_infos_complementares.js
+++ js/tela_infos_complementares.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', function() {
     img_check2 = document.querySelector('.check2')
     img_check3 = document.querySelector('.check3')
 
+    botao_finalizar = document.querySelector('.finalizar_infos')
+
     if (localStorage.getItem('check1') == '1'){
         img_check1.style.display = 'block'
     }
@@ -14,4 +16,8 @@ document.addEventListener('DOMContentLoaded', function() {
         img_check3.style.display = 'block'
     }
 
+    if (localStorage.getItem('check1') == '1' && localStorage.getItem('check2') == '1' && localStorage.getItem('check3') == '1'){
+        botao_finalizar.style.opacity = '1'
+        botao_finalizar.disabled = false
+    }
 })
