0d9828589d51a76b4ecd3f7ccdb757a147d0c98c
rodrigogcoelhoo <rodrigogc4@al.insper.edu.br>
2020-12-01 23:05:37 -0300

    Erros arrumados

--- js/tela_infos_evento.js
+++ js/tela_infos_evento.js
@@ -1,11 +1,20 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-    ps = document.querySelectorAll('.infos_atualizadas')
-    local = ps[0]
-    horario = ps[1]
-    convidados = ps[2]
+    div = document.querySelectorAll('.box_evento_info')
 
-    local.innerHTML = localStorage.getItem('local')
-    horario.innerHTML = localStorage.getItem('horario')
-    convidados.innerHTML = localStorage.getItem('convidados')
+    local_h2 = document.createElement('h2')
+    local_h2.className = "infos_atualizadas";
+    local_h2.innerHTML = localStorage.getItem('local')
+    div[0].appendChild(local_h2)
+
+    horario_h2 = document.createElement('h2')
+    horario_h2.className = "infos_atualizadas";
+    horario_h2.innerHTML = localStorage.getItem('horario')
+    div[1].appendChild(horario_h2)
+
+    convidados_h2 = document.createElement('h2')
+    convidados_h2.className = "infos_atualizadas";
+    convidados_h2.innerHTML = localStorage.getItem('convidados')
+    div[2].appendChild(convidados_h2)
+
 })
